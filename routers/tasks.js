4b5843871a79adadd2a8da7ebed2abf75ce45756
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-13 02:00:50 +0530

    Changed router to avoid chaining

--- routers/tasks.js
+++ routers/tasks.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require("../controllers/tasks")
 
-router.route("/").get(getAllTasks).post(createTask)
+router.get("/",getAllTasks)
+router.post("/",createTask)
 
-router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask)
+router.get("/:id",getTask)
+router.patch("/:id",updateTask)
+router.delete("/:id",deleteTask)
 
 module.exports=  router
